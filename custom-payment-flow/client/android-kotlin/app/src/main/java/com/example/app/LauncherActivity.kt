f028f4090d18a4d7862a3ea0af79bb1357fc02f7
Hibariya <hibariya@gmail.com>
2021-08-22 14:42:37 +0900

    wip

--- custom-payment-flow/client/android-kotlin/app/src/main/java/com/example/app/LauncherActivity.kt
+++ custom-payment-flow/client/android-kotlin/app/src/main/java/com/example/app/LauncherActivity.kt
@@ -16,7 +16,8 @@ import java.io.IOException
 import com.stripe.android.PaymentConfiguration
 
 // 10.0.2.2 is the Android emulator's alias to localhost
-val BackendUrl = "http://10.0.2.2:4242/"
+//val BackendUrl = "http://10.0.2.2:4242/"
+val BackendUrl = "http://web:4242/"
 
 class LauncherActivity : AppCompatActivity() {
 
