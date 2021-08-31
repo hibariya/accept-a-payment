2bfab391727eb7740f2a4061283846da32942b4c
Hibariya <hibariya@gmail.com>
2021-08-31 21:34:48 +0900

    wip

--- .github/workflows/android_e2e.sh
+++ .github/workflows/android_e2e.sh
@@ -4,23 +4,22 @@ cd custom-payment-flow/client/android-kotlin
 ./gradlew installDebug
 cd -
 
-eval "$(docker-machine env default)"
-source sample-ci/helpers.sh
-
-install_docker_compose_settings
-export STRIPE_WEBHOOK_SECRET=$(retrieve_webhook_secret)
-cat <<EOF >> .env
+export STRIPE_WEBHOOK_SECRET=$(stripe listen --api-key $STRIPE_SECRET_KEY --print-secret)
+cat <<EOF >> custom-payment-flow/server/go/.env
 DOMAIN="$SERVER_URL"
 PRICE=${PRICE}
 PAYMENT_METHOD_TYPES="card"
-APPUIM_SERVER_URL="http://host.docker.internal:4723/wd/hub"
+APPUIM_SERVER_URL="http://localhost:4723/wd/hub"
 APPIUM_APK_PATH=/home/runner/work/accept-a-payment/accept-a-payment/custom-payment-flow/client/android-kotlin/app/build/outputs/apk/debug/app-debug.apk
 EOF
 
-configure_docker_compose_for_integration custom-payment-flow node ../../client/html node:14.17
-# TODO: FIXME
-docker-compose up -d && docker-compose exec -T runner bash -c 'curl -I --retry 30 --retry-delay 3 --retry-connrefused http://web:4242'
-command="docker-compose exec -T runner bundle exec rspec spec/custom_payment_flow_android_spec.rb"
+stripe listen --forward-to http://localhost:4242/webhook &
+
+cd custom-payment-flow/server/go
+go run server.go &
+
+bundle install -j5
+command="bundle exec rspec spec/custom_payment_flow_android_spec.rb"
 $command \
   || $command --only-failures \
   || $command --only-failures --format RSpec::Github::Formatter --format progress
