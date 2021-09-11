69d36459827c1ce032c817508747f0bb80043765
Hibariya <hibariya@gmail.com>
2021-09-11 09:57:01 +0900

    wip

--- .github/workflows/android_e2e.sh
+++ .github/workflows/android_e2e.sh
@@ -27,16 +27,6 @@ $command \
   || $command --only-failures \
   || $command --only-failures --format RSpec::Github::Formatter --format progress
 
-echo adb devices
-adb devices
-echo adb devices -l
-adb devices -l
-echo aoeu
-adb shell am force-stop com.stripe.android.testapp || true
-echo adb -s emulator-5554 emu kill
-adb kill-server
-adb -s emulator-5554 emu kill
-
 status=$?
 kill $(jobs -p)
 exit $status
