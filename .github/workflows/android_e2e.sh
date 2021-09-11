5ddf64e08d8b8954313bb1da9a4210217220061a
Hibariya <hibariya@gmail.com>
2021-09-11 22:39:28 +0900

    wip

--- .github/workflows/android_e2e.sh
+++ .github/workflows/android_e2e.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -xe
 
-cd custom-payment-flow/client/android-kotlin
-./gradlew installDebug
-cd -
-
-appium &>/dev/null &
-stripe listen --forward-to http://localhost:4242/webhook &
-
-export STRIPE_WEBHOOK_SECRET=$(stripe listen --api-key $STRIPE_SECRET_KEY --print-secret)
-cat <<EOF >> custom-payment-flow/server/go/.env
-DOMAIN="$SERVER_URL"
-PRICE=${PRICE}
-PAYMENT_METHOD_TYPES="card"
-EOF
-
-export APPUIM_SERVER_URL="http://localhost:4723/wd/hub"
-export APPIUM_APK_PATH=/Users/runner/work/accept-a-payment/accept-a-payment/custom-payment-flow/client/android-kotlin/app/build/outputs/apk/debug/app-debug.apk
-
-cd custom-payment-flow/server/go
-go run server.go &
-cd -
+#cd custom-payment-flow/client/android-kotlin
+#./gradlew installDebug
+#cd -
+#
+#appium &>/dev/null &
+#stripe listen --forward-to http://localhost:4242/webhook &
+#
+#export STRIPE_WEBHOOK_SECRET=$(stripe listen --api-key $STRIPE_SECRET_KEY --print-secret)
+#cat <<EOF >> custom-payment-flow/server/go/.env
+#DOMAIN="$SERVER_URL"
+#PRICE=${PRICE}
+#PAYMENT_METHOD_TYPES="card"
+#EOF
+#
+#export APPUIM_SERVER_URL="http://localhost:4723/wd/hub"
+#export APPIUM_APK_PATH=/Users/runner/work/accept-a-payment/accept-a-payment/custom-payment-flow/client/android-kotlin/app/build/outputs/apk/debug/app-debug.apk
+#
+#cd custom-payment-flow/server/go
+#go run server.go &
+#cd -
 
 #mkdir -p tmp
 #command="bundle exec rspec spec/custom_payment_flow_android_spec.rb"
@@ -31,6 +31,6 @@ cd -
 adb shell am start -a android.intent.action.VIEW -d https://www.stackoverflow.com
 sleep 5
 
-status=$?
-kill $(jobs -p)
-exit $status
+#status=$?
+#kill $(jobs -p)
+#exit $status
