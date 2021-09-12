ae207fcee0b22d1c1dfa4a4af1e25fdbdd6b8400
Hibariya <hibariya@gmail.com>
2021-09-12 18:29:02 +0900

    wip

--- .github/workflows/android_e2e.sh
+++ .github/workflows/android_e2e.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -xe
 
 cd custom-payment-flow/client/android-kotlin
-./gradlew installDebug
-#./gradlew assembleDebug
+./gradlew assembleDebug
 cd -
 
 appium &>/dev/null &
 stripe listen --forward-to http://localhost:4242/webhook &
 
 export STRIPE_WEBHOOK_SECRET=$(stripe listen --api-key $STRIPE_SECRET_KEY --print-secret)
-#cat <<EOF >> custom-payment-flow/server/go/.env
 cat <<EOF >> custom-payment-flow/server/java/.env
 DOMAIN="$SERVER_URL"
 PRICE=${PRICE}
@@ -20,9 +18,6 @@ EOF
 export APPUIM_SERVER_URL="http://localhost:4723/wd/hub"
 export APPIUM_APK_PATH=/Users/runner/work/accept-a-payment/accept-a-payment/custom-payment-flow/client/android-kotlin/app/build/outputs/apk/debug/app-debug.apk
 
-#cd custom-payment-flow/server/go
-#go run server.go &
-#cd -
 cd custom-payment-flow/server/java
 mvn package
 java -cp target/sample-jar-with-dependencies.jar com.stripe.sample.Server &
@@ -35,26 +30,4 @@ $command \
   || $command --only-failures \
   || $command --only-failures --format RSpec::Github::Formatter --format progress
 
-#adb shell am start -n com.example.app/com.example.app.LauncherActivity
-#sleep 10
-
-#adb shell am force-stop com.example.app
-#sleep 10
-#adb uninstall com.example.app
-#sleep 10
-#adb -e reboot
-#sleep 90
-
-#adb shell am start -a android.intent.action.VIEW -d https://www.stackoverflow.com
-#sleep 5
-
-#status=$?
 kill $(jobs -p)
-#exit $status
-
-#set +e
-#
-#ps -ef | grep -i gradle
-#
-#cd custom-payment-flow/client/android-kotlin
-#./gradlew --stop
