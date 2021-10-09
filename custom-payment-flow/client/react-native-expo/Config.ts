52c45dae4410c15b3902c417c60bcc9476699125
Hibariya <hibariya@gmail.com>
2021-10-09 22:40:35 +0900

    wip

--- custom-payment-flow/client/react-native-expo/Config.ts
+++ custom-payment-flow/client/react-native-expo/Config.ts
@@ -3,6 +3,8 @@ import ENV from 'react-native-config';
 
 // Address to stripe server running on local machine
 export const LOCAL_URL =
-  Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
+  process.env.SERVER_URL
+    ? process.env.SERVER_URL
+    : Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
 
 export const API_URL = ENV.API_URL ? ENV.API_URL : LOCAL_URL;
