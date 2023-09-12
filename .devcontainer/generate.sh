4d1bb6fec5ffc092cd368e6d8fee3456b4ec07f4
Hibariya <hibariya@gmail.com>
2023-09-12 14:23:20 +0900

    wip

--- .devcontainer/generate.sh
+++ .devcontainer/generate.sh
@@ -4,14 +4,27 @@ source ../sample-ci/helpers.sh
 
 export SAMPLE=${1}
 export SERVER_TYPE=${2}
-export STATIC_DIR=${3}
+export STATIC_DIR=${3:-../../client/html}
 export SERVER_IMAGE=${4}
-export WORKING_DIR="${SAMPLE}/server/${SERVER_TYPE}"
+
+if [ "$STATIC_DIR" = "../../client/html" ]
+then
+  export WORKING_DIR="${SAMPLE}/server/${SERVER_TYPE}"
+  export SERVICE="web"
+  export RUN_SERVICES='["runner"]'
+  export PORT=4242
+else
+  export WORKING_DIR="${SAMPLE}/client/$(basename "$STATIC_DIR")"
+  export SERVICE="frontend"
+  export RUN_SERVICES='["runner", "frontend"]'
+  export PORT=3000
+fi
+
 
 CONFIG_DIR=$(echo "$WORKING_DIR" | tr / -)
 mkdir -p "$CONFIG_DIR"
-cat devcontainer-template.json | envsubst '$WORKING_DIR' > "$CONFIG_DIR/devcontainer.json"
-cp docker-compose.override.yml "$CONFIG_DIR/docker-compose.override.yml"
+cat devcontainer-template.json | envsubst '$WORKING_DIR $SERVICE $RUN_SERVICES $PORT' > "$CONFIG_DIR/devcontainer.json"
+cat docker-compose.override.yml | envsubst '$STATIC_DIR' > "$CONFIG_DIR/docker-compose.override.yml"
 
 pushd "$CONFIG_DIR"
 ln -sF ../../sample-ci .
